73bdbbabb54403227ba4903441ca6d4578d247f9
Alex <alexcris.m@gmail.com>
2016-05-24 18:39:21 -0500

    cerrar sesion

--- src/components/index/index.jsx
+++ src/components/index/index.jsx
@@ -1,10 +1,13 @@
 'use strict';
 
+require('./index.scss');
+
 // -----------------------------------------------------------------------------------------------
 // React + Other Modules
 // -----------------------------------------------------------------------------------------------
 
 var React = require('react');
+var Parse = require('parse');
 
 // -----------------------------------------------------------------------------------------------
 // Index
@@ -15,8 +18,16 @@ var Index = React.createClass({
         return (
             <main className='index'>
                 <h2>PITAGORAS</h2>
+                <button type='button' onClick={this.signOut}>Cerrar sesión</button>
             </main>
         );
+    },
+    signOut: function (event) {
+        event.preventDefault();
+
+        Parse.User.logOut().then(function () {
+            window.location = '/signin';
+        });
     }
 });
 
