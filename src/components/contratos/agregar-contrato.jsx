79139c4a70e969271d6129f649c193c1a09eae24
Eduardo Cristerna <eduardo@schoolhub.com>
2016-05-30 18:29:58 -0500

    basic save

--- src/components/contratos/agregar-contrato.jsx
+++ src/components/contratos/agregar-contrato.jsx
@@ -7,18 +7,64 @@ require('./agregar-contrato.scss');
 // -----------------------------------------------------------------------------------------------
 
 var React = require('react');
+var Parse = require('parse');
+var Contrato = Parse.Object.extend("Contrato");
 
 // -----------------------------------------------------------------------------------------------
 // Agregar Contrato
 // -----------------------------------------------------------------------------------------------
 
 var AgregarContrato = React.createClass({
+    getInitialState: function () {
+        return {
+            numeroContrato: null,
+            plazo: null,
+            monto: null,
+            tasa: null
+        };
+    },
     render: function () {
         return (
-            <main className='agregar-contrato'>
+            <div className='addcontract-wrapper'>
                 <h2>Agregar Contrato</h2>
-            </main>
+                <form onSubmit={this.handleAddContract}>
+                    <label>Número de Contrato</label>
+                    <input type='text' value={this.state.numeroContrato} onChange={this.handleChange.bind(this, 'numeroContrato')} />
+                    <label>Plazo</label>
+                    <input type='text' value={this.state.plazo} onChange={this.handleChange.bind(this, 'plazo')} />
+                    <label>Monto</label>
+                    <input type='text' value={this.state.monto} onChange={this.handleChange.bind(this, 'monto')} />
+                    <label>Tasa</label>
+                    <input type='text' value={this.state.tasa} onChange={this.handleChange.bind(this, 'tasa')} />
+                    <button type='submit'>Agregar Contrato</button>
+                </form>
+            </div>
         );
+    },
+    handleChange: function (propertyName, event) {
+        var state = {};
+        state[propertyName] = event.target.value;
+
+        this.setState(state);
+    },
+    handleAddContract: function (event) {
+        event.preventDefault();
+
+        var newContract = new Contrato();
+
+        newContract.save({
+            numeroContrato: this.state.numeroContrato,
+            plazo: Number(this.state.plazo),
+            monto: Number(this.state.monto),
+            tasa: Number(this.state.tasa)
+        }, {
+            success: function(newContract) {
+                alert('New object created with objectId: ' + newContract.id);
+            },
+            error: function(newContract, error) {
+                alert('Failed to create new object, with error code: ' + error.message);
+            }
+        });
     }
 });
 
