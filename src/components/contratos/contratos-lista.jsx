7080540b9090c0f6558f3ca77704392c8d78b85a
Eduardo Cristerna <eduardo@schoolhub.com>
2016-06-01 19:48:21 -0500

    table

--- src/components/contratos/contratos-lista.jsx
+++ src/components/contratos/contratos-lista.jsx
@@ -14,8 +14,39 @@ var ContratosLista = React.createClass({
     render: function () {
         return (
             <main className='contratos-lista'>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Número de Contrato</th>
+                            <th>Monto</th>
+                            <th>Plazo</th>
+                            <th>Tasa</th>
+                        </tr>
+                        {this.renderContratos()}
+                    </tbody>
+                </table>
             </main>
         );
+    },
+    renderContratos: function () {
+        if (!this.props.contratos) {
+            return;
+        }
+
+        var contratos = [];
+
+        this.props.contratos.forEach(function (contrato) {
+            contratos.push(
+                <tr key={contrato.id}>
+                    <td>{contrato.numeroContrato}</td>
+                    <td>{contrato.monto}</td>
+                    <td>{contrato.plazo}</td>
+                    <td>{contrato.tasa}</td>
+                </tr>
+            );
+        });
+
+        return contratos;
     }
 });
 
