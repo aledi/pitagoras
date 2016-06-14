f459f6c60de27e3e1ccdc66c13090bd06a9973df
carotorrehdz <cdelatorre@Carolinas-MacBook-Pro.local>
2016-06-14 18:56:28 -0500

    Visita

--- src/components/acciones/visita.jsx
+++ src/components/acciones/visita.jsx
@@ -11,9 +11,36 @@ var React = require('react');
 // -----------------------------------------------------------------------------------------------
 
 var Visita = React.createClass({
+    getInitialState: function () {
+        return {
+            domicilioUbicado: false,
+            clienteUbicado: false,
+            datosDeContacto: null
+        };
+    },
     render: function () {
         return (
-            <div>Visita</div>
+            <div className='visita'>
+                    <div>
+                        <p>¿Encontró el domicilio?</p>
+                        <input type='radio' id='domicilioUbicado' />
+                        <label htmlFor='domicilioUbicado'>Sí</label>
+                        <input type='radio' id='domicilioNoUbicado' />
+                        <label htmlFor='domicilioNoUbicado'>No</label>
+                    </div>
+                    <div>
+                        <p>¿Ubicó al cliente?</p>
+                        <input type='radio' id='clienteUbicado' />
+                        <label htmlFor='clienteUbicado'>Sí</label>
+                        <input type='radio' id='clienteNoUbicado' />
+                        <label htmlFor='clienteNoUbicado'>No</label>
+                    </div>
+                    <div>
+                        <p>Datos del Contacto</p>
+                        <label>¿Con quién dejó el comunicado?</label>
+                        <input type='text' />
+                    </div>
+            </div>
         );
     }
 });
