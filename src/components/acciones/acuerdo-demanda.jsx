46a626dc82600d331b85ab6fc8a27ab36bb3b43c
Alex <alexcris.m@gmail.com>
2016-06-14 18:53:32 -0500

    acuerdo-demanda

--- src/components/acciones/acuerdo-demanda.jsx
+++ src/components/acciones/acuerdo-demanda.jsx
@@ -13,7 +13,21 @@ var React = require('react');
 var AcuerdoDemanda = React.createClass({
     render: function () {
         return (
-            <div>AcuerdoDemanda</div>
+            <div className='acuerdo-demanda'>
+                <p>Resultado del acuerdo</p>
+                <div>
+                    <input type='radio' id='desecha' />
+                    <label htmlFor='desecha'>Desecha</label>
+                </div>
+                <div>
+                    <input type='radio' id='previene' />
+                    <label htmlFor='previene'>Previene</label>
+                </div>
+                <div>
+                    <input type='radio' id='admite' />
+                    <label htmlFor='admite'>Admite</label>
+                </div>
+            </div>
         );
     }
 });
