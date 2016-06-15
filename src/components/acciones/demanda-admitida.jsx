1dbede12ce1356ae62212a4a1a1b229c598a8d83
Alex <alexcris.m@gmail.com>
2016-06-15 18:25:05 -0500

    demanda admitida

--- src/components/acciones/demanda-admitida.jsx
+++ src/components/acciones/demanda-admitida.jsx
@@ -13,7 +13,24 @@ var React = require('react');
 var DemandaAdmitida = React.createClass({
     render: function () {
         return (
-            <div>DemandaAdmitida</div>
+            <div className='demanda-admitida'>
+                <p>Tipo de Juicio</p>
+                <div>
+                    <input type='radio' id='oral' />
+                    <label htmlFor='oral'>Oral Mercantil</label>
+                </div>
+                <div>
+                    <input type='radio' id='ejecutiva' />
+                    <label htmlFor='ejecutiva'>Ejecutiva Mercantil</label>
+                </div>
+
+                <label>Número de Registro</label>
+                <input type='text' />
+                <label>Juzgado</label>
+                <input type='text' />
+                <label>Fecha de Presentación</label>
+                <input type='text' />
+            </div>
         );
     }
 });
