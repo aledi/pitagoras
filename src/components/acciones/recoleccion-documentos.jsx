40a5cf3171b6e4bb07d3de1527aaf8f684a4ca5e
carotorrehdz <cdelatorre@Carolinas-MacBook-Pro.local>
2016-06-15 18:24:43 -0500

    correciones recoleccion-documentos

--- src/components/acciones/recoleccion-documentos.jsx
+++ src/components/acciones/recoleccion-documentos.jsx
@@ -16,10 +16,14 @@ var RecoleccionDocumentos = React.createClass({
             <div className='recoleccion-documentos'>
                 <div>
                     <p>¿Documentos recogidos?</p>
-                    <input type='radio' id='documentosRecogidos' />
-                    <label htmlFor='documentosRecogidos'>Sí</label>
-                    <input type='radio' id='documentosNoRecogidos' />
-                    <label htmlFor='documentosNoRecogidos'>No</label>
+                    <div>
+                        <input type='radio' id='si' />
+                        <label htmlFor='si'>Si</label>
+                    </div>
+                    <div>
+                        <input type='radio' id='no' />
+                        <label htmlFor='no'>No</label>
+                    </div>
                 </div>
             </div>
         );
