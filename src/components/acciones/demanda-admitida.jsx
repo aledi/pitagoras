d354b1a01cbcde294e5ff7176bb8d8506bd0c907
carotorrehdz <cdelatorre@Carolinas-MacBook-Pro.local>
2016-06-16 19:21:19 -0500

    demanda-admitida

--- src/components/acciones/demanda-admitida.jsx
+++ src/components/acciones/demanda-admitida.jsx
@@ -23,27 +23,57 @@ var DemandaAdmitida = React.createClass({
         };
     },
     render: function () {
+        var respuestas = this.state.respuestas;
+
         return (
             <div className='demanda-admitida'>
                 <p>Tipo de Juicio</p>
                 <div>
-                    <input type='radio' id='oral' />
+                    <input
+                        type='radio'
+                        id='oral'
+                        value='oralMercantil'
+                        checked={respuestas.tipoJuicio === 'oralMercantil'}
+                        onChange={this.handleRadioChange} />
                     <label htmlFor='oral'>Oral Mercantil</label>
                 </div>
                 <div>
-                    <input type='radio' id='ejecutiva' />
+                    <input
+                        type='radio'
+                        id='ejecutiva'
+                        value='ejectivaMercantil'
+                        checked={respuestas.tipoJuicio === 'ejectivaMercantil'}
+                        onChange={this.handleRadioChange} />
                     <label htmlFor='ejecutiva'>Ejecutiva Mercantil</label>
                 </div>
-
                 <label>Número de Registro</label>
-                <input type='text' />
+                <input
+                    type='text'
+                    value={respuestas.numeroRegistro}
+                    onChange={this.handleInputChange.bind(this, 'numeroRegistro')} />
                 <label>Juzgado</label>
-                <input type='text' />
+                <input
+                    type='text'
+                    value={respuestas.juzgado}
+                    onChange={this.handleInputChange.bind(this, 'juzgado')} />
                 <label>Fecha de Presentación</label>
-                <input type='text' />
+                <input
+                    type='text'
+                    value={respuestas.fechaPresentacion}
+                    onChange={this.handleInputChange.bind(this, 'fechaPresentacion')} />
                 {this.renderButton()}
             </div>
         );
+    },
+    handleRadioChange: function (event) {
+        var respuestas = this.state.respuestas;
+        respuestas.tipoJuicio = event.target.value;
+        this.setState({respuestas: respuestas});
+    },
+    handleInputChange: function (propertyName, event) {
+        var respuestas = this.state.respuestas;
+        respuestas[propertyName] = event.target.value;
+        this.setState({respuestas: respuestas});
     }
 });
 
