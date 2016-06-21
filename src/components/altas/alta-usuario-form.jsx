de66a90330efdd2957ff9725b2502d0e57ce0db7
Eduardo Cristerna <eduardo@schoolhub.com>
2016-06-21 10:06:10 -0500

    fixes

--- src/components/altas/alta-usuario-form.jsx
+++ src/components/altas/alta-usuario-form.jsx
@@ -19,9 +19,9 @@ var AltaUsuarioForm = React.createClass({
     getInitialState: function () {
         return {
             nombre: null,
-            apellidos: null,
+            apellido: null,
             email: null,
-            tipo: null,
+            tipo: 1,
             username: null,
             password: null
         };
@@ -33,31 +33,31 @@ var AltaUsuarioForm = React.createClass({
                     <p className='section-title'>Nuevo Usuario</p>
                     <div className='input-wrapper'>
                         <label>Nombre</label>
-                        <input type='text' onChange={this.handleChange.bind(this, 'nombre')} />
+                        <input type='text' value={this.state.nombre} onChange={this.handleChange.bind(this, 'nombre')} />
                     </div>
                     <div className='input-wrapper'>
                         <label>Apellidos</label>
-                        <input type='text' onChange={this.handleChange.bind(this, 'apellidos')} />
+                        <input type='text' value={this.state.apellido} onChange={this.handleChange.bind(this, 'apellido')} />
                     </div>
                     <div className='input-wrapper'>
                         <label>Correo Electroónico</label>
-                        <input type='text' onChange={this.handleChange.bind(this, 'email')} />
+                        <input type='text' value={this.state.email} onChange={this.handleChange.bind(this, 'email')} />
                     </div>
                     <div className='input-wrapper'>
                         <label>Tipo de Usuario</label>
-                        <select onChange={this.handleChange.bind(this, 'tipo')}>
-                            <option value={3}>Administrador</option>
-                            <option value={2}>Abogado</option>
+                        <select value={this.state.tipo} onChange={this.handleChange.bind(this, 'tipo')}>
                             <option value={1}>Gestor</option>
+                            <option value={2}>Abogado</option>
+                            <option value={3}>Administrador</option>
                         </select>
                     </div>
                     <div className='input-wrapper'>
                         <label>Nombre de Usuario</label>
-                        <input type='text' onChange={this.handleChange.bind(this, 'username')} />
+                        <input type='text' value={this.state.username} onChange={this.handleChange.bind(this, 'username')} />
                     </div>
                     <div className='input-wrapper'>
                         <label>Contraseña Provisional</label>
-                        <input type='text' onChange={this.handleChange.bind(this, 'password')} />
+                        <input type='text' value={this.state.password} onChange={this.handleChange.bind(this, 'password')} />
                     </div>
                     <button type='submit'>Agregar Usuario</button>
                 </form>
@@ -69,26 +69,27 @@ var AltaUsuarioForm = React.createClass({
         state[propertyName] = event.target.value;
 
         this.setState(state);
+        console.log(this.state);
     },
     handleAltaUsuario: function (event) {
         event.preventDefault();
 
         var user = new UserObject();
 
-        user.set('nombre', this.state.nombre);
-        user.set('apellidos', this.state.apellidos);
-        user.set('email', this.state.email);
-        user.set('tipo', parseInt(this.state.tipo, 10));
-        user.set('username', this.state.username);
-        user.set('password', this.state.password);
-        user.set('usuarioNuevo', true);
+        user.set({
+            nombre: this.state.nombre,
+            apellido: this.state.apellidos,
+            email: this.state.email,
+            tipo: parseInt(this.state.tipo, 10),
+            username: this.state.username,
+            password: this.state.password,
+            usuarioNuevo: true
+        });
 
         user.signUp(null, {
             success: function(user) {
-                alert("Success " + user.id);
             },
             error: function(user, error) {
-                alert("Error: " + error.code + " " + error.message);
             }
         });
     }
