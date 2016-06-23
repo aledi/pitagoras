78950746933f94fffca987db9c19a7acb9b28d19
Alex <alexcris.m@gmail.com>
2016-06-23 15:43:26 -0500

    demanda-prevenida

--- src/components/respuestas/demanda-prevenida-rsp.jsx
+++ src/components/respuestas/demanda-prevenida-rsp.jsx
@@ -16,8 +16,23 @@ var DemandaPrevenidaRespuestas = React.createClass({
     render: function () {
         return (
             <div className='respuestas-wrapper'>
-                <span className='bold'>¿Desahogar?: </span>
-                <span>{RespuestasUtils.formatBooleanRespuesta(this.props.accion.respuestas.desahogar)}</span>
+                <div>
+                    <span className='bold'>¿Desahogar?: </span>
+                    <span>{RespuestasUtils.formatBooleanRespuesta(this.props.accion.respuestas.desahogar)}</span>
+                </div>
+                {this.renderFecha()}
+            </div>
+        );
+    },
+    renderFecha: function () {
+        if (!this.props.accion.respuestas.desahogar) {
+            return;
+        }
+
+        return (
+            <div className='respuestas-wrapper'>
+                <span className='bold'>Desahogar el: </span>
+                <span>{this.props.accion.respuestas.fecha}</span>
             </div>
         );
     }
