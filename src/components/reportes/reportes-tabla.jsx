076b8b5dd5e7b528659a4a3505b1cb90ec54c645
Alex <alexcris.m@gmail.com>
2016-07-28 20:49:48 -0500

    progress

--- src/components/reportes/reportes-tabla.jsx
+++ src/components/reportes/reportes-tabla.jsx
@@ -21,8 +21,25 @@ var ReportesTabla = React.createClass({
     render: function () {
         return (
             <div className='reportes-table table-wrapper'>
+                <table id='example'>
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th>
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+</table>
                 <div className='table-header-wrapper' style={{minWidth: tableMinWidth}}>
-                    <table>
+                    <table id='reportes'>
                         <thead>
                             <tr className='header'>
                                 <th style={{width: '250px'}}>
@@ -99,8 +116,23 @@ var ReportesTabla = React.createClass({
                     </table>
                 </div>
                 {this.renderTableBodyWrapper()}
+                <a id='dlink' style={{display: 'none'}} />
+                <button type='button' className='export' onClick={this.tableToExcel}>Export</button>
             </div>
         );
+    },
+    tableToExcel: function () {
+        var uri = 'data:application/vnd.ms-excel;base64,'
+        , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+        , base64 = function (s) { return window.btoa(unescape(encodeURIComponent(s))) }
+        , format = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) }
+
+        var table = document.getElementById('example');
+        var ctx = {worksheet: 'reportes' || 'Worksheet', table: table.innerHTML};
+
+        document.getElementById('dlink').href = uri + base64(format(template, ctx));
+        document.getElementById('dlink').download = 'reportes.xls';
+        document.getElementById('dlink').click();
     },
     renderTableBodyWrapper: function () {
         return (
