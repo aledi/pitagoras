8caa0a2bc73db3a0ee85f519ba2cc190faa3bc0a
Alex <alexcris.m@gmail.com>
2016-08-06 17:41:25 -0500

    pitagoras

--- src/server.js
+++ src/server.js
@@ -1,19 +1,15 @@
 'use strict';
 
 var express = require('express');
-var path = require('path');
-
 var app = express();
-
+var pitagoras = require('./pitagoras');
 var PORT = process.env.PORT || 5000;
-
-// Server configuration
 
 app.set('port', PORT);
 
 // For avoidong Heroku $PORT error
-app.use(require('src'));
-
-app.listen(PORT, function () {
+app.get('/', function (request, response) {
+    response.send(pitagoras);
+}).listen(PORT, function () {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
