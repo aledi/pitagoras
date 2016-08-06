fb8bf391a6624343f160c1b31d90ce88b85e4ad1
Alex <alexcris.m@gmail.com>
2016-08-06 17:24:05 -0500

    src

--- src/server.js
+++ src/server.js
@@ -7,15 +7,12 @@ var app = express();
 
 var PORT = process.env.PORT || 5000;
 
-var srcPath = path.join(__dirname, "src");
-
 // Server configuration
 
-app.set("views", srcPath);
 app.set('port', PORT);
 
 // For avoidong Heroku $PORT error
-app.use('/', express.static(path.join(__dirname, '..', 'build')));
+app.use(require('src'));
 
 app.listen(PORT, function () {
     console.log('App is running, server is listening on port ', app.get('port'));
