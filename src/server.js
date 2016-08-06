1f2592d70a1696ebf409070bf62eb048474e1794
Alex <alexcris.m@gmail.com>
2016-08-06 17:09:57 -0500

    use

--- src/server.js
+++ src/server.js
@@ -1,15 +1,22 @@
 'use strict';
 
 var express = require('express');
+var path = require('path');
+
 var app = express();
+
 var PORT = process.env.PORT || 5000;
 
+var srcPath = path.join(__dirname, "src");
+
+// Server configuration
+
+app.set("views", srcPath);
 app.set('port', PORT);
 
 // For avoidong Heroku $PORT error
-app.get('/', function (request, response) {
-    var result = 'App is running';
-    response.send(result);
-}).listen(PORT, function () {
+app.use('/', express.static(path.join(__dirname, '..', 'build')));
+
+app.listen(PORT, function () {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
