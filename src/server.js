bb861099b8fde4acba0f82ba74ffa4170ef8bf2d
Alex <alexcris.m@gmail.com>
2016-08-06 17:57:52 -0500

    result

--- src/server.js
+++ src/server.js
@@ -3,14 +3,13 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 5000;
-var ksd = require('./src/index.html');
 
 app.set('port', PORT);
 
 // For avoidong Heroku $PORT error
 app.get('/', function (request, response) {
     var result = 'App is running';
-    response.send(ksd);
+    response.send(result);
 }).listen(PORT, function () {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
