99f807bb3143f8e2f5ddbfe91806cd9c8f9f7c43
Alex <alexcris.m@gmail.com>
2016-08-06 17:49:01 -0500

    file

--- src/server.js
+++ src/server.js
@@ -2,14 +2,14 @@
 
 var express = require('express');
 var app = express();
-var pitagoras = require('./index.html');
+var pitagoras = require('index.html');
 var PORT = process.env.PORT || 5000;
 
 app.set('port', PORT);
 
 // For avoidong Heroku $PORT error
 app.get('/', function (request, response) {
-    response.send(pitagoras);
+    response.sendFile(pitagoras);
 }).listen(PORT, function () {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
