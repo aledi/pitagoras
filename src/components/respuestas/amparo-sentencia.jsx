7c2ae87bc4cbdfaf56f6ce365e91565b9bb40954
Alex <alexcris.m@gmail.com>
2016-12-28 11:19:03 -0600

    date

--- src/components/respuestas/amparo-sentencia.jsx
+++ src/components/respuestas/amparo-sentencia.jsx
@@ -5,6 +5,7 @@
 // -----------------------------------------------------------------------------------------------
 
 var React = require('react');
+var DateUtils = require('src/utils/date-utils');
 
 // -----------------------------------------------------------------------------------------------
 // Amparo vs Sentencia
@@ -20,6 +21,20 @@ var AmparoSentencia = React.createClass({
                     <span className='bold'>{juicioEjecutiva ? 'Favorable a: ' : 'Promovido por: '}</span>
                     <span>{juicioEjecutiva ? this.props.accion.respuestas.favorable : this.props.accion.respuestas.promovido}</span>
                 </div>
+                {this.renderDate(juicioEjecutiva)}
+            </div>
+        );
+    },
+    renderDate: function (juicioEjecutiva) {
+        var respuestas = this.props.accion.respuestas;
+        if (!juicioEjecutiva || !respuestas.fecha) {
+            return;
+        }
+
+        return (
+            <div>
+                <span className='bold'>Fecha: </span>
+                <span>{DateUtils.formatFechaRespuesta(respuestas.fecha)}</span>
             </div>
         );
     }
