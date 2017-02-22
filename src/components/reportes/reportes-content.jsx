ee08861493242a3f0997c50c0a74495947df3497
Alex <alexcris.m@gmail.com>
2017-02-22 11:19:40 -0600

    fix

--- src/components/reportes/reportes-content.jsx
+++ src/components/reportes/reportes-content.jsx
@@ -40,20 +40,39 @@ var ReportesContent = React.createClass({
     toggleReportes: function () {
         this.setState({showingReportesGenerales: !this.state.showingReportesGenerales});
     },
+    // exportTable: function () {
+    //     var date = new Date();
+    //     var tipoReportes = this.state.showingReportesGenerales ? 'reportes' : 'extrajudiciales';
+    //     var uri = 'data:application/vnd.ms-excel;base64,';
+    //     var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta charset="utf-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>';
+    //     var table = document.getElementById(tipoReportes);
+    //     var ctx = {
+    //         worksheet: tipoReportes,
+    //         table: table.innerHTML
+    //     };
+    //
+    //     document.getElementById('dlink').href = uri + this.base64(this.format(template, ctx));
+    //     document.getElementById('dlink').download = tipoReportes + '-pitagoras-' + date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear() + '.xls';
+    //     document.getElementById('dlink').click();
+    // },
     exportTable: function () {
-        var date = new Date();
         var tipoReportes = this.state.showingReportesGenerales ? 'reportes' : 'extrajudiciales';
-        var uri = 'data:application/vnd.ms-excel;base64,';
         var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><meta charset="utf-8"><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>';
         var table = document.getElementById(tipoReportes);
         var ctx = {
             worksheet: tipoReportes,
             table: table.innerHTML
         };
-
-        document.getElementById('dlink').href = uri + this.base64(this.format(template, ctx));
-        document.getElementById('dlink').download = tipoReportes + '-pitagoras-' + date.getDate() + '-' + (date.getMonth() + 1) + '-' + date.getFullYear() + '.xls';
-        document.getElementById('dlink').click();
+        var myBlob = new Blob([this.format(template, ctx)], {type: 'application/vnd.ms-excel'});
+        var url = window.URL.createObjectURL(myBlob);
+        var a = document.createElement('a');
+        document.body.appendChild(a);
+        a.href = url;
+        a.download = 'newfile.xls';
+        a.click();
+        setTimeout(function () {
+            window.URL.revokeObjectURL(url);
+        }, 0);
     },
     base64: function (s) {
         return window.btoa(unescape(encodeURIComponent(s)));
