0d029c90d2aa592ee203ac688569de91983fe48b
Alex <alexcris.m@gmail.com>
2017-03-30 10:57:22 -0600

    acuerdo-demanda

--- src/components/acciones/acuerdo-demanda.jsx
+++ src/components/acciones/acuerdo-demanda.jsx
@@ -27,7 +27,11 @@ var AcuerdoDemanda = React.createClass({
             respuestas: {
                 resultadoAcuerdo: lastAccion ? lastAccion.respuestas.resultadoAcuerdo : 'Desecha',
                 fechaAcuerdo: lastAccion ? lastAccion.respuestas.fechaAcuerdo : null,
-                fechaPublicacion: lastAccion ? lastAccion.respuestas.fechaPublicacion : null
+                fechaPublicacion: lastAccion ? lastAccion.respuestas.fechaPublicacion : null,
+                numeroFactura: lastAccion ? lastAccion.respuestas.numeroFactura : '',
+                localExhorto: lastAccion ? lastAccion.respuestas.localExhorto : 'Local',
+                disposicion: lastAccion ? lastAccion.respuestas.disposicion : null,
+                diligenciable: lastAccion ? lastAccion.respuestas.diligenciable : null
             },
             disabled: false
         };
@@ -49,7 +53,7 @@ var AcuerdoDemanda = React.createClass({
                             id='desecha'
                             value='Desecha'
                             checked={this.state.respuestas.resultadoAcuerdo === 'Desecha'}
-                            onChange={this.handleChange}
+                            onChange={this.handleChange.bind(this, 'resultadoAcuerdo')}
                             disabled={this.state.disabled} />
                         <label htmlFor='desecha'>Desecha</label>
                     </div>
@@ -59,7 +63,7 @@ var AcuerdoDemanda = React.createClass({
                             id='previene'
                             value='Previene'
                             checked={this.state.respuestas.resultadoAcuerdo === 'Previene'}
-                            onChange={this.handleChange} />
+                            onChange={this.handleChange.bind(this, 'resultadoAcuerdo')} />
                         <label htmlFor='previene' disabled={this.state.disabled}>Previene</label>
                     </div>
                     <div>
@@ -68,10 +72,11 @@ var AcuerdoDemanda = React.createClass({
                             id='admite'
                             value='Admite'
                             checked={this.state.respuestas.resultadoAcuerdo === 'Admite'}
-                            onChange={this.handleChange}
+                            onChange={this.handleChange.bind(this, 'resultadoAcuerdo')}
                             disabled={this.state.disabled} />
                         <label htmlFor='admite' disabled={this.state.disabled}>Admite</label>
                     </div>
+                    {this.renderNumeroFactura()}
                 </div>
                 <div className='element-wrapper'>
                     <h5>Fecha de acuerdo</h5>
@@ -81,14 +86,126 @@ var AcuerdoDemanda = React.createClass({
                     <h5>Fecha de publicación</h5>
                     <DateSelect date={this.state.respuestas.fechaPublicacion} onChange={this.handleFechaChange.bind(this, 'fechaPublicacion')} />
                 </div>
+                <div className='element-wrapper'>
+                    <h5>Local o Exhorto</h5>
+                    <div>
+                        <input
+                            type='radio'
+                            id='local'
+                            value='Local'
+                            checked={this.state.respuestas.localExhorto === 'Local'}
+                            onChange={this.handleChange.bind(this, 'localExhorto')}
+                            disabled={this.state.disabled} />
+                        <label htmlFor='local'>Local</label>
+                    </div>
+                    <div>
+                        <input
+                            type='radio'
+                            id='exhorto'
+                            value='Exhorto'
+                            checked={this.state.respuestas.localExhorto === 'Exhorto'}
+                            onChange={this.handleChange.bind(this, 'localExhorto')} />
+                        <label htmlFor='exhorto' disabled={this.state.disabled}>Exhorto</label>
+                    </div>
+                    {this.renderRadios()}
+                </div>
                 {this.renderComentarios()}
                 {this.renderButton()}
             </div>
         );
     },
-    handleChange: function (event) {
+    renderNumeroFactura: function () {
+        if (this.state.respuestas.resultadoAcuerdo !== 'Admite') {
+            return;
+        }
+
+        return (
+            <div>
+                <h5 className='text-label'>Número de Factura</h5>
+                <input
+                    type='text'
+                    value={this.state.respuestas.numeroFactura}
+                    onChange={this.handleChange.bind(this, 'numeroFactura')}
+                    disabled={this.state.disabled} />
+            </div>
+        );
+    },
+    renderRadios: function () {
+        if (this.state.respuestas.localExhorto !== 'Exhorto') {
+            return;
+        }
+
+        return (
+            <div>
+                <h5>Disposición</h5>
+                <div>
+                    <input
+                        type='radio'
+                        id='disposicion'
+                        value='A disposición'
+                        checked={this.state.respuestas.disposicion === 'A disposición'}
+                        onChange={this.handleChange.bind(this, 'disposicion')}
+                        disabled={this.state.disabled} />
+                    <label htmlFor='disposicion'>A disposición</label>
+                </div>
+                <div>
+                    <input
+                        type='radio'
+                        id='juzgado'
+                        value='Envía juzgado'
+                        checked={this.state.respuestas.disposicion === 'Envía juzgado'}
+                        onChange={this.handleChange.bind(this, 'disposicion')} />
+                    <label htmlFor='juzgado' disabled={this.state.disabled}>Envía juzgado</label>
+                </div>
+                {this.renderDisposicionRadios()}
+            </div>
+        );
+    },
+    renderDisposicionRadios: function () {
+        if (this.state.respuestas.disposicion !== 'A disposición') {
+            return;
+        }
+
+        return (
+            <div>
+                <h5>Diligenciable</h5>
+                <div>
+                    <input
+                        type='radio'
+                        id='diligenciable'
+                        value='Diligenciable'
+                        checked={this.state.respuestas.diligenciable === 'Diligenciable'}
+                        onChange={this.handleChange.bind(this, 'diligenciable')}
+                        disabled={this.state.disabled} />
+                    <label htmlFor='diligenciable'>Diligenciable</label>
+                </div>
+                <div>
+                    <input
+                        type='radio'
+                        id='jurisdiccion'
+                        value='Fuera de Jurisdicción'
+                        checked={this.state.respuestas.diligenciable === 'Fuera de Jurisdicción'}
+                        onChange={this.handleChange.bind(this, 'diligenciable')} />
+                    <label htmlFor='jurisdiccion' disabled={this.state.disabled}>Fuera de Jurisdicción</label>
+                </div>
+            </div>
+        );
+    },
+    handleChange: function (key, event) {
         var respuestas = this.state.respuestas;
-        respuestas.resultadoAcuerdo = event.target.value;
+        var value = event.target.value;
+
+        if (key === 'resultadoAcuerdo' && value !== 'Admite') {
+            respuestas.numeroFactura = '';
+        } else if (key === 'localExhorto' && value === 'Local') {
+            respuestas.disposicion = null;
+            respuestas.diligenciable = null;
+        } else if (key === 'disposicion' && value === 'Envía juzgado') {
+            respuestas.diligenciable = null;
+        }
+
+        respuestas[key] = value;
+
         this.setState({respuestas: respuestas});
     },
     handleFechaChange: function (fecha, date) {
--- src/components/respuestas/acuerdo-demanda.jsx
+++ src/components/respuestas/acuerdo-demanda.jsx
@@ -22,8 +22,51 @@ var AcuerdoDemanda = React.createClass({
                     <span className='bold'>Resultado del acuerdo: </span>
                     <span>{respuestas.resultadoAcuerdo}</span>
                 </div>
+                {this.renderNumeroFactura()}
+                <div>
+                    <span className='bold'>Local o Exhorto: </span>
+                    <span>{respuestas.localExhorto}</span>
+                </div>
                 {this.renderDate(respuestas.fechaAcuerdo, 'Fecha de acuerdo')}
                 {this.renderDate(respuestas.fechaPublicacion, 'Fecha de publicación')}
+                {this.renderDisposicion()}
+                {this.renderDiligenciable()}
+            </div>
+        );
+    },
+    renderNumeroFactura: function () {
+        if (!this.props.accion.respuestas.numeroFactura) {
+            return;
+        }
+
+        return (
+            <div>
+                <span className='bold'>Número de Factura: </span>
+                <span>{this.props.accion.respuestas.numeroFactura}</span>
+            </div>
+        );
+    },
+    renderDisposicion: function () {
+        if (this.props.accion.respuestas.localExhorto === 'Local') {
+            return;
+        }
+
+        return (
+            <div>
+                <span className='bold'>Disposición: </span>
+                <span>{this.props.accion.respuestas.disposicion}</span>
+            </div>
+        );
+    },
+    renderDiligenciable: function () {
+        if (this.props.accion.respuestas.disposicion !== 'A disposición') {
+            return;
+        }
+
+        return (
+            <div>
+                <span className='bold'>Diligenciable: </span>
+                <span>{this.props.accion.respuestas.diligenciable}</span>
             </div>
         );
     },
